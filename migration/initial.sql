2739a3a3017259c6f66a2d5fcef3fb5a31dc72b3
wendy <wendy.wendy@dimo.co.id>
2020-04-22 23:57:56 +0700

    add migration initialization.

---
+++ migration/initial.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS `flip`;
+
+CREATE DATABASE `flip`;
+
+USE `flip`;
+
+DROP TABLE IF EXISTS `disbursement`;
+
+CREATE TABLE `disbursement` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
+  `updated` datetime DEFAULT NULL,
+  `response_id` bigint(20) unsigned NOT NULL,
+  `response_amount` double NOT NULL,
+  `response_timestamp` datetime NOT NULL,
+  `response_bank_code` varchar(100) NOT NULL,
+  `response_account_number` varchar(100) NOT NULL,
+  `response_beneficiary_name` varchar(255) NOT NULL,
+  `response_remark` text,
+  `response_receipt` text,
+  `response_time_served` datetime DEFAULT NULL,
+  `response_fee` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
